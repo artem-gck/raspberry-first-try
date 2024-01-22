c46a10676a059115be4986b2fabd1ec6d802e777
agats <agats@ARTEM_PC>
2024-01-22 22:05:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+const string MyCORSOrigins = "myCORSOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyCORSOrigins,
+                      policy => policy.AllowAnyHeader()
+                                      .AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                     );
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(MyCORSOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
